ac12b3adec03c1dc039b1b87f9e9516641c22f0f
Alexander Shvets <neochief@shvetsgroup.com>
2017-01-09 15:44:18 +0200

    Update components.ps1

--- components.ps1
+++ components.ps1
@@ -3,7 +3,6 @@
 Push-Location (Join-Path (Split-Path -parent $profile) "components")
 
 # From within the ./components directory...
-. .\coreaudio.ps1
-. .\githubforwindows.ps1
+#. .\coreaudio.ps1
 
 Pop-Location
